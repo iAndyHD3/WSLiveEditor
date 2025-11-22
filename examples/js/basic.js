6e442cf797349e7ec77e9a2ce74cd42d745f1f8d
iAndyHD3 <54410739+iAndyHD3@users.noreply.github.com>
2025-11-22 23:26:09 +0100

    move to libhv websocket

--- examples/js/basic.js
+++ examples/js/basic.js
@@ -1,24 +1,34 @@
-const WebSocket = require('ws');
-const socket = new WebSocket('ws://127.0.0.1:1313');
+const WebSocket = require("ws");
+const socket = new WebSocket("ws://127.0.0.1:1313");
 
-socket.addEventListener('message', (event) => {
-  console.log('Message from server:', event.data);
+socket.addEventListener("message", (event) => {
+  console.log("Message from server:", event.data);
 });
 
-socket.addEventListener('open', (event) => {
-	const del = {
-		action: 'REMOVE',
-		group: 9999,
-	};
-	socket.send(JSON.stringify(del));
-	
-	//few hardcoded x positions
-	const array = [45, 75, 105, 135, 165];
-	const randomElement = array[Math.floor(Math.random() * array.length)];
-	const add = {
-		action: 'ADD_OBJECTS',
-		objects: "1,1,2," + randomElement + ",3,75,57,1.9999",
-		close: true
-	};
-	socket.send(JSON.stringify(add));
+socket.addEventListener("close", (event) => {
+  console.log("Disconnecting from the server");
+});
+
+socket.addEventListener("open", (event) => {
+  console.log("Connected to the server");
+
+  const del = {
+    action: "REMOVE_OBJECTS",
+    group: 9999,
+  };
+  socket.send(JSON.stringify(del));
+
+  const add = {
+    action: "ADD_OBJECTS",
+    objects: "1,1,2,60,3,90,57,1.9999",
+  };
+
+  socket.send(JSON.stringify(add));
+
+  socket.send(
+    JSON.stringify({
+      action: "GET_LEVEL_STRING",
+      close: true,
+    }),
+  );
 });
--- src/LiveServer.cpp
+++ src/LiveServer.cpp
@@ -1,79 +1,237 @@
-#include <FlexibleServer.hpp>
-#include "ActionResponse.hpp"
-#include "Geode/loader/Loader.hpp"
-#include "actions/Add.hpp"
-#include "actions/Remove.hpp"
-#include "actions/GetLevelString.hpp"
-#include "actions/GetSelectedObjects.hpp"
+
+
+#include "hv/WebSocketChannel.h"
 
 #include <Geode/Geode.hpp>
+#include <Geode/binding/EditorUI.hpp>
 #include <Geode/binding/GJGameLevel.hpp>
+#include <Geode/binding/GameManager.hpp>
 #include <Geode/binding/LevelEditorLayer.hpp>
 #include <Geode/loader/Log.hpp>
 #include <matjson.hpp>
 #include <string_view>
 
 #include <Geode/modify/LevelEditorLayer.hpp>
+#include <Geode/modify/MenuLayer.hpp>
+#include <Geode/modify/CCDirector.hpp>
+#include <Geode/modify/GameManager.hpp>
+
 #include <fmt/format.h>
+#include <proxy/proxy.h>
+#include "ActionUtils.hpp"
 
-#include <ZeroMQServer.hpp>
-#include <std23/function_ref.h>
-#include <thread>
+#include <glaze/glaze.hpp>
+#include <glaze/thread/shared_async_vector.hpp>
+#include <hv/WebSocketServer.h>
 
-std::string onMessage(std::string_view msg);
-std::unique_ptr<ZmqIpcServer> zmqServer;
+hv::WebSocketServer server;
+hv::WebSocketService ws;
+
+struct Response {
+    std::string_view status; //this is only ever set to compile-time strings
+    std::string error;
+    std::optional<glz::generic> response;
+    static Response make_success() {
+        return {.status = "successful"};
+    }
 
 
-std::string handleAction(const matjson::Value& action)
+    static Response make_success(glz::generic&& payload) noexcept {
+        Response res;
+        res.status = "successful";
+        res.response.emplace(std::move(payload));  // move into the optional
+        return res;
+    }
+
+    static Response make_error(std::string&& error_msg) {
+        return {.status = "error", .error = std::move(error_msg)};
+    }
+};
+
+PRO_DEF_MEM_DISPATCH(MemRun, run);
+
+struct Runnable : pro::facade_builder
+    ::add_convention<MemRun, Response(LevelEditorLayer*)>
+    ::build {};
+
+
+
+struct Action
 {
-    auto action_type = action.get<std::string>("action");
-    if(!action_type) return ActionResponse::make_error("no action found").get();
-
-#define DO_ACTION_CHECK(FN_ACTION_TYPE) \
-    if(FN_ACTION_TYPE::ACTION_TYPE == *action_type && FN_ACTION_TYPE::isValid(action))\
-        return FN_ACTION_TYPE::run(LevelEditorLayer::get(), action).get()
-    
-    DO_ACTION_CHECK(AddObjectsAction);
-    DO_ACTION_CHECK(RemoveObjects);
-    DO_ACTION_CHECK(GetLevelString);
+    WebSocketChannelPtr client;
+    pro::proxy<Runnable> runner;
+    bool shouldClose = false;
+};
 
 
-
-    return ActionResponse::make_error("No matching action found").get();
-}
-
-//runs on polling thread still
-std::string onMessage(std::string_view msg)
+struct Add
 {
-    auto json = matjson::parse(msg);
-    if(!json)
+    static constexpr auto ACTION_NAME = "ADD_OBJECTS";
+    static constexpr auto EDITOR_ACTION = true;
+    std::string action;
+    std::string objects;
+    bool close;
+    Response run(LevelEditorLayer* editor)
     {
-        auto err = json.unwrapErr();
-        return ActionResponse::make_error(fmt::format("{}:{} :", err.line, err.column, err.message)).get();
+        EditorUI::get()->m_alertShown = true;
+        editor->createObjectsFromString(objects, false, false);
+        return Response::make_success();
     }
-    std::string response;
-    std::atomic<bool> response_ready = false;
+};
 
-    
-    geode::Loader::get()->queueInMainThread([&](){
-        response = handleAction(json.unwrap());
-        response_ready.store(true);
-    });
+GLZ_ACTION_META(Add)
 
-    while(!response_ready.load())
+static gd::vector<short> getGroupIDs(GameObject* obj) {
+    gd::vector<short> res;
+
+    if (obj->m_groups && obj->m_groups->at(0))
+        for (auto i = 0; i < obj->m_groupCount; i++)
+            res.push_back(obj->m_groups->at(i));
+    return res;
+}
+static bool hasGroup(GameObject* obj, int group)
+{
+    for (const auto& g : getGroupIDs(obj))
     {
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        if (g == group)
+            return true;
     }
-    return response;
+    return false;
 }
 
-$on_mod(Loaded) {
-    try {
-    zmqServer = std::make_unique<ZmqIpcServer>("iandyhd3-wsliveeditor", onMessage);
-    zmqServer->start();
-    } catch(std::exception e) {
-        geode::log::error("{}", e.what());
-        return;
+
+
+//remove
+struct Remove
+{
+    static constexpr auto ACTION_NAME = "REMOVE_OBJECTS";
+    static constexpr auto EDITOR_ACTION = true;
+    std::string action;
+    int group;
+    bool close;
+    Response run(LevelEditorLayer* editor)
+    {
+        geode::cocos::CCArrayExt<GameObject*> toDelete;
+        for(GameObject* obj : geode::cocos::CCArrayExt<GameObject*>(editor->m_objects))
+        {
+            if (hasGroup(obj, group))
+            {
+                toDelete.push_back(obj);
+            }
+        }
+
+        if(toDelete.size() == 0) return Response::make_success();
+
+
+        auto selected = editor->m_editorUI->getSelectedObjects();
+        editor->m_editorUI->deselectAll();
+
+        editor->m_editorUI->selectObjects(toDelete.inner(), false);
+
+        editor->m_editorUI->onDeleteSelected(nullptr);
+
+        editor->m_editorUI->selectObjects(selected, false);
+        return Response::make_success();
     }
-        geode::log::error("all good");
+};
+
+
+
+GLZ_ACTION_META(Remove)
+
+//GetLevelString
+struct GetLevelString
+{
+    static constexpr auto ACTION_NAME = "GET_LEVEL_STRING";
+    static constexpr auto EDITOR_ACTION = true;
+    std::string action;
+    bool close;
+    Response run(LevelEditorLayer* editor)
+    {
+        glz::generic s = std::string(editor->getLevelString());
+        return Response::make_success(std::move(s));
+    }
+};
+
+
+
+std::vector<Action> g_actions;
+std::mutex g_actionsMutex;
+
+std::atomic<bool> g_inEditor;
+
+
+
+
+$on_mod(Loaded)
+{
+    ws.onopen = [](const WebSocketChannelPtr& channel, const HttpRequestPtr& req)
+    {
+        geode::log::debug("open");
+    };
+    ws.onmessage = [](const WebSocketChannelPtr& channel, const std::string& msg)
+    {
+        geode::log::debug("recieved: {}", msg);
+
+
+        CHECK_ACTION(Add)
+        CHECK_ACTION(Remove)
+        CHECK_ACTION(GetLevelString)
+    };
+
+    ws.onclose = [](const WebSocketChannelPtr& channel) {
+        geode::log::debug("onclose");
+    };
+
+    server = hv::WebSocketServer(&ws);
+    server.setPort(1313);
+    server.setThreadNum(1);
+    server.start();
+
 }
+
+
+struct LSHooks : geode::Modify<LSHooks, LevelEditorLayer>
+{
+    struct Fields {
+        ~Fields() {
+            g_inEditor = false;
+        }
+    };
+
+    void performQueuedActions(float dt)
+    {
+        std::lock_guard lock(g_actionsMutex);
+        if(g_actions.empty()) return;
+
+        for(auto&[client, runner, shouldClose] : g_actions)
+        {
+            //geode::log::info("hi");
+            if(client->isClosed()) return;
+
+            if(auto resp = glz::write_json(runner->run(this)))
+            {
+                client->send(*resp);
+            }
+            else
+            {
+                client->send("{\"status\":\"error\",\"error\":\"Could not produce response object\"}");
+            }
+            if(shouldClose)
+            {
+                client->close();
+            }
+        }
+        g_actions.clear();
+    }
+
+    bool init(GJGameLevel* level, bool idk)
+    {
+        if(!LevelEditorLayer::init(level, idk)) return false;
+        g_inEditor = true;
+        this->schedule(schedule_selector(LSHooks::performQueuedActions), 0.1f);
+        return true;
+    }
+
+
+};
